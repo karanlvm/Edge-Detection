d44d619881069274049775e54c78e7f31e0867ce
Karan <karanlvm123@gmail.com>
2021-10-09 20:53:29 +0530

    OpenCV added
    Had to manually add OpenCv to the project cause android studio doesn't allow the regular method anymore.

---
+++ app/src/main/java/com/example/imageprocessing/CameraActivity.java
@@ -0,0 +1,4 @@
+package com.example.imageprocessing;
+
+public class CameraActivity {
+}
--- app/src/main/java/com/example/imageprocessing/MainActivity.java
+++ app/src/main/java/com/example/imageprocessing/MainActivity.java
@@ -3,8 +3,24 @@ package com.example.imageprocessing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+    // To check if OpenCV is loaded or not in Logcat
+    private static String TAG = "MainActivity";
+    static
+    {
+        if(OpenCVLoader.initDebug())
+        {
+            Log.d(TAG, "OpenCV installed");
+        }
+        else
+        {
+            Log.d(TAG, "OpenCv failed");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
